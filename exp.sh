0660131cd3fb482facaea25f8837a74642001a5a
jiayu-ch15 <jiayu-ch15@mails.tsinghua.edu.cn>
2020-02-10 17:19:09 +0800

    config

--- exp.sh
+++ exp.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=7 python main.py simple_adversary MA_MA --batch_size 516 \
+CUDA_VISIBLE_DEVICES=7 python main.py simple_adversary D_D --batch_size 516 \
  --n_episodes 50000 --n_exploration_eps 50000 --agent_alg DDPG --adversary_alg DDPG
 
 
